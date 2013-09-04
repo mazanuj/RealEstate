601cf4c520a2bb99aabe72a02cd74192ce4bc5a3
Artem Khotsianovich <a.khotsianovich@dev1team.net>
2013-09-04 19:58:26 +0300

    advert

--- RealEstate/Entities/Advert.cs
+++ RealEstate/Entities/Advert.cs
@@ -11,8 +11,8 @@ namespace RealEstate.Entities
         public string Url { get; set; }
 
         public string Title { get; set; }
-        public string Name { get; set; }
 
+        public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
