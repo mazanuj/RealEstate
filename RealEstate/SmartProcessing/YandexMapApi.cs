7ff03ff68f34c266e008f5baf7900fc560a9d1d8
ktflabs <ktf.labs@gmail.com>
2013-10-22 02:43:42 +0300

    start yandex api

---
+++ RealEstate/SmartProcessing/YandexMapApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace RealEstate.SmartProcessing
+{
+    public class YandexMapApi
+    {
+        public string SearchObject(string Address)
+        {
+            string urlXml = "http://geocode-maps.yandex.ru/1.x/?geocode=" + Address + "&results=1";
+            WebClient client = new WebClient();
+            string result = client.DownloadString(urlXml);
+            return result;
+        }
+    }
+}
